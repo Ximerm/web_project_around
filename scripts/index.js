115b144809a34c8a8edafbd06eccedece5275cba
XimenaRodriguez <ximerm138@gmail.com>
2025-01-09 17:18:20 -0500

    Update project for first submission

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,8 @@ let closePopupButton = document.querySelector(".popup__close");
 let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__hobbie");
 
+let formElement = document.querySelector(".popup__form");
+
 function openEditPopup(evt) {
   popup.classList.add("popup_opened");
 
@@ -13,22 +15,22 @@ function openEditPopup(evt) {
   inputJob.value = profileAbout.textContent;
 }
 
-function renderAdded() {
-  let profileName = document.querySelector(".profile__name");
-  let profileAbout = document.querySelector(".profile__hobbie");
-
-  if (profileName.value === "" || profileAbout.value === "") {
-    submitButton.setAttribute("disable", true);
-    submitButton.classList.add("popup__form-submit_disable");
-  } else {
-    submitButton.removeAttribute("disable");
-    submitButton.classList.remove("popup__form-submit_disable");
-  }
+function closeEditPopup(evt) {
+  popup.classList.remove("popup_opened");
 }
 
-function closeEditPopup(evt) {
+function FormSubmit(evt) {
+  evt.preventDefault();
+  let NameInput = document.querySelector("#inputName");
+  let JobInput = document.querySelector("#inputJob");
+
+  {
+    profileName.textContent = NameInput.value;
+    profileAbout.textContent = JobInput.value;
+  }
   popup.classList.remove("popup_opened");
 }
 
 editButton.addEventListener("click", openEditPopup);
 closePopupButton.addEventListener("click", closeEditPopup);
+formElement.addEventListener("submit", FormSubmit);
