49bdefe8132c7749e053ae0f2b501e0a8974946d
XimenaRodriguez <ximerm138@gmail.com>
2025-01-09 11:09:21 -0500

    Submit Button code

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,28 @@ let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let closePopupButton = document.querySelector(".popup__close");
 
-let formValues = document.querySelector(".popup__form-input");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__hobbie");
 
 function openEditPopup(evt) {
   popup.classList.add("popup_opened");
-  let formValues = document.getElementById("input-Name");
+
+  let id = evt.currentTarget.id;
+  inputName.value = profileName.textContent;
+  inputJob.value = profileAbout.textContent;
+}
+
+function renderAdded() {
+  let profileName = document.querySelector(".profile__name");
+  let profileAbout = document.querySelector(".profile__hobbie");
+
+  if (profileName.value === "" || profileAbout.value === "") {
+    submitButton.setAttribute("disable", true);
+    submitButton.classList.add("popup__form-submit_disable");
+  } else {
+    submitButton.removeAttribute("disable");
+    submitButton.classList.remove("popup__form-submit_disable");
+  }
 }
 
 function closeEditPopup(evt) {
