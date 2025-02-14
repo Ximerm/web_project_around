6ca5dcecd96ecffe25a42ee8ba1a20d0971323c8
XimenaRodriguez <ximerm138@gmail.com>
2025-02-13 20:18:21 -0500

    Q&A

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,83 @@
-let editButton = document.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
-let closePopupButton = document.querySelector(".popup__close");
+const editButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__hobbie");
+const addButton = document.querySelector(".profile__add-button");
 
-let profileName = document.querySelector(".profile__name");
-let profileAbout = document.querySelector(".profile__hobbie");
+const cardsContainer = document.querySelector(".card__element");
+const cardTemplate = document.querySelector("#card-template").content;
 
-let formElement = document.querySelector(".popup__form");
+const popup = document.querySelector(".popup");
+const closePopupButton = document.querySelector(".popup__close");
+const popupFormTitle = document.querySelector(".popup__form-title");
+const popupFormButton = document.querySelector(".popup__form-submit");
+const firstInput = document.querySelector("#inputFirst");
+const secondInput = document.querySelector("#inputSecond");
 
-function openEditPopup(evt) {
+const formElement = document.querySelector(".popup__form");
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+//Create cards
+initialCards.forEach((card) => {
+  let cardTemplate = createCard(card);
+  cardsContainer.prepend(cardTemplate);
+});
+
+function createCard(card) {
+  let cardElm = cardTemplate.querySelector(".card__content").cloneNode(true);
+
+  cardElm.querySelector(".card__photo").src = card.link;
+  cardElm.querySelector(".card__photo-name").textContent = card.name;
+
+  return cardElm;
+}
+
+//Abrir, cerrar y enviar Popup
+function openEditAddPopup(evt) {
+  const className = evt.currentTarget.className;
+
+  switch (className) {
+    case "profile__edit-button":
+      popupFormTitle.textContent = "Editar perfil";
+      popupFormButton.textContent = "Guardar";
+      firstInput.placeholder = "Nombre";
+      secondInput.placeholder = "Acerca de mí";
+      firstInput.value = profileName.textContent;
+      secondInput.value = profileAbout.textContent;
+      break;
+    case "profile__add-button":
+      popupFormTitle.textContent = "Nuevo lugar";
+      popupFormButton.textContent = "Crear";
+      firstInput.placeholder = "Título";
+      secondInput.placeholder = "Enlace a la imagen";
+      break;
+  }
   popup.classList.add("popup_opened");
-
-  let id = evt.currentTarget.id;
-  inputName.value = profileName.textContent;
-  inputJob.value = profileAbout.textContent;
 }
 
 function closeEditPopup(evt) {
@@ -21,16 +86,28 @@ function closeEditPopup(evt) {
 
 function FormSubmit(evt) {
   evt.preventDefault();
-  let NameInput = document.querySelector("#inputName");
-  let JobInput = document.querySelector("#inputJob");
+  const buttonText = evt.currentTarget.innerText;
 
-  {
-    profileName.textContent = NameInput.value;
-    profileAbout.textContent = JobInput.value;
+  switch (buttonText) {
+    case "Guardar":
+      profileName.textContent = firstInput.value;
+      profileAbout.textContent = secondInput.value;
+      firstInput.value = "";
+      secondInput.value = "";
+      break;
+    case "Crear":
+      const card = {
+        name: firstInput.value,
+        link: secondInput.value,
+      };
+      let cardElem = createCard(card);
+      cardsContainer.prepend(cardElem);
+      break;
   }
   popup.classList.remove("popup_opened");
 }
 
-editButton.addEventListener("click", openEditPopup);
+editButton.addEventListener("click", openEditAddPopup);
+addButton.addEventListener("click", openEditAddPopup);
 closePopupButton.addEventListener("click", closeEditPopup);
 formElement.addEventListener("submit", FormSubmit);
